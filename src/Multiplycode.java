e8c21e7a489ceb9c4e7084afb8b24792d3e96f37
chiragnarayanakere <chirag.narayanakere@gmail.com>
2018-10-05 10:36:12 -0700

    first send

---
+++ src/Multiplycode.java
@@ -0,0 +1,21 @@
+
+public class Multiplycode {
+
+	public static void main(String[] args) {
+		
+		int first = 5;
+		int second = 6;
+		int product = 0;
+		
+		product = mult(first, second);
+		
+		
+		System.out.println(product);
+	}
+	
+	public static int mult(int one, int two) {
+		
+		return (one * two);
+	}
+
+}
